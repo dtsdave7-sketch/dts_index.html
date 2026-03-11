3adf352b5acf6347df4dae4660ea883f048d63d1
dtsdave7-sketch <dtsdave7@gmail.com>
2026-03-11 12:20:07 -0400

    Add files via upload

---
+++ product.js
@@ -0,0 +1,15 @@
+<div class="product-page">
+
+<img id="image">
+
+<h1 id="name"></h1>
+
+<p id="price"></p>
+
+<p id="desc"></p>
+
+<button>Add to cart</button>
+
+</div>
+
+<script src="js/product.js"></script>
